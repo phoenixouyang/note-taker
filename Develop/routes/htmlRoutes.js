49cb88170be216422cf19289a47c01057160a6f8
Pholobo <ouyangphoenix@gmail.com>
2023-03-12 15:39:18 -0400

    adds GET routes for htmlRoutes.js

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+// require dependencies
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// GET routes
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
