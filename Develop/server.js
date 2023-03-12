cdef00266229836ad8fc0f1813cc3dc68d738b48
Pholobo <ouyangphoenix@gmail.com>
2023-03-12 15:05:10 -0400

    adds routes and updates server.js file for dependencies and route usage

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+// require Express.js
+const express = require('express');
+
+// specify port that Express.js server will run
+const PORT = process.env.PORT || 3001;
+
+// initialize instance of Express.js
+const app = express();
+
+// require routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// middleware to parse and url encode incoming data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+// use api routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// listen to incoming connections
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}. Happy note taking!`)
+);
