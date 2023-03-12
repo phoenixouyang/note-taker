07bcbcc6fa5c657906f2608517dca30e7ef6eaa0
Pholobo <ouyangphoenix@gmail.com>
2023-03-12 16:12:00 -0400

    adds new router file notes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,4 +7,13 @@ router.get('/api/notes', (req, res) =>
     readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
-router.post('/api/notes')
+router.post('/api/notes', (req, res) => {
+    const newNote = {
+        id: uuid(),
+        title: req.body.title,
+        text: req.body.text,
+    };
+    readAndAppend(res.json(newNote), '../db/db.json');
+});
+
+module.exports = router;
